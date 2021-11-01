b07b778b996e9394cb82e81c7598e7a13088440d
40923111 <40923111@gm.nfu.edu.tw>
2021-11-01 10:00:38 +0800

    Rearrangement

---
+++ downloads/安裝.js
@@ -0,0 +1,30 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "2": [
+      { type: "gotoframe", rect: { x: 100, y: 166, width: 28, height: 13 }, target: 3 },
+    ],
+    "10": [
+      { type: "gotoframe", rect: { x: 240, y: 201, width: 28, height: 13 }, target: 2 },
+      { type: "gotoframe", rect: { x: 296, y: 200, width: 28, height: 13 }, target: 11 },
+    ],
+    "48": [
+      { type: "gotoframe", rect: { x: 85, y: 53, width: 28, height: 13 }, target: 10 },
+      { type: "gotoframe", rect: { x: 141, y: 52, width: 28, height: 13 }, target: 49 },
+    ],
+    "98": [
+      { type: "gotoframe", rect: { x: 255, y: 424, width: 28, height: 13 }, target: 48 },
+      { type: "gotoframe", rect: { x: 336, y: 423, width: 28, height: 13 }, target: 99 },
+    ],
+    "139": [
+      { type: "gotoframe", rect: { x: 559, y: 501, width: 28, height: 13 }, target: 98 },
+      { type: "gotoframe", rect: { x: 659, y: 500, width: 28, height: 13 }, target: 140 },
+    ],
+    "153": [
+      { type: "gotoframe", rect: { x: 570, y: 531, width: 28, height: 13 }, target: 139 },
+    ],
+  },
+};
