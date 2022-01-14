30602ce9918f4a0d745c8786fb2a8f4bfc720936
40923111 <40923111@gm.nfu.edu.tw>
2022-01-14 19:02:33 +0800

    final exam

---
+++ downloads/robot.js
@@ -0,0 +1,47 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "2": [
+      { type: "gotoframe", rect: { x: 701, y: 499, width: 60, height: 24 }, target: 3 },
+    ],
+    "41": [
+      { type: "gotoframe", rect: { x: 422, y: 186, width: 60, height: 24 }, target: 2 },
+      { type: "gotoframe", rect: { x: 526, y: 185, width: 60, height: 24 }, target: 42 },
+    ],
+    "66": [
+      { type: "gotoframe", rect: { x: 187, y: 400, width: 60, height: 24 }, target: 41 },
+      { type: "gotoframe", rect: { x: 580, y: 396, width: 60, height: 24 }, target: 67 },
+    ],
+    "73": [
+      { type: "gotoframe", rect: { x: 460, y: 369, width: 60, height: 24 }, target: 66 },
+      { type: "gotoframe", rect: { x: 715, y: 369, width: 60, height: 24 }, target: 74 },
+    ],
+    "121": [
+      { type: "gotoframe", rect: { x: 321, y: 455, width: 60, height: 24 }, target: 73 },
+      { type: "gotoframe", rect: { x: 590, y: 456, width: 60, height: 24 }, target: 122 },
+    ],
+    "161": [
+      { type: "gotoframe", rect: { x: 416, y: 314, width: 60, height: 24 }, target: 121 },
+      { type: "gotoframe", rect: { x: 579, y: 155, width: 60, height: 24 }, target: 162 },
+    ],
+    "186": [
+      { type: "gotoframe", rect: { x: 678, y: 277, width: 60, height: 24 }, target: 161 },
+      { type: "gotoframe", rect: { x: 762, y: 275, width: 60, height: 24 }, target: 187 },
+    ],
+    "215": [
+      { type: "gotoframe", rect: { x: 385, y: 121, width: 60, height: 24 }, target: 186 },
+      { type: "gotoframe", rect: { x: 500, y: 123, width: 60, height: 24 }, target: 216 },
+    ],
+    "275": [
+      { type: "gotoframe", rect: { x: 219, y: 104, width: 60, height: 24 }, target: 215 },
+      { type: "gotoframe", rect: { x: 427, y: 103, width: 60, height: 24 }, target: 276 },
+    ],
+    "292": [
+      { type: "gotoframe", rect: { x: 599, y: 472, width: 60, height: 24 }, target: 275 },
+      { type: "gotoframe", rect: { x: 738, y: 468, width: 60, height: 24 }, target: 293 },
+    ],
+  },
+};
